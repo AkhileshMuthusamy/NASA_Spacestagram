5551de77c276207fadb54582db6fb21f953d8663
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-01-12 20:24:06 -0500

    Reverted previous change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-// import {ServiceWorkerModule} from '@angular/service-worker';
-// import {environment} from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FilterModalComponent} from './dialog-boxes/filter-modal/filter-modal.component';
@@ -43,12 +43,12 @@ import {SafeUrlPipe} from './shared/pipes/safe-url.pipe';
     FlexLayoutModule,
     HttpClientModule,
     ReactiveFormsModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: environment.production,
-    //   // Register the ServiceWorker as soon as the app is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
